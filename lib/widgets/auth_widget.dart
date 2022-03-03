7b94b00fa9f7f7bf5724b174c23040c14d933e4e
MPREIJI <mpreiji@gmail.com>
2022-03-03 22:55:02 +0600

    Update auth_widget.dart

--- lib/widgets/auth_widget.dart
+++ lib/widgets/auth_widget.dart
@@ -39,18 +39,16 @@ class _HeaderWidgetState extends State<_HeaderWidget> {
       padding: const EdgeInsets.symmetric(horizontal: 16),
       child: Column(
         children: [
-          const SizedBox(
-            height: 25,
-          ),
+          const SizedBox(height: 50),
+          const _FormWidget(),
+          const SizedBox(height: 25),
           const Text(
             'Чтобы пользоваться правкой и возможностями рейтинга TMDB, а также получить персональные'
             ' рекомендации, необходимо войти в свою учётную запись. Если у вас нет учётной записи, её регистрация '
             'является бесплатной и простой. ',
             style: textStyle,
           ),
-          const SizedBox(
-            height: 25,
-          ),
+          const SizedBox(height: 25),
           const Text(
             'Если Вы зарегистрировались, но не получили письмо для подтверждения, нажмите здесь, чтобы '
             'отправить письмо повторно.',
@@ -58,6 +56,43 @@ class _HeaderWidgetState extends State<_HeaderWidget> {
           ),
         ],
       ),
+    );
+  }
+}
+
+class _FormWidget extends StatefulWidget {
+  const _FormWidget({Key? key}) : super(key: key);
+
+  @override
+  State<_FormWidget> createState() => _FormWidgetState();
+}
+
+class _FormWidgetState extends State<_FormWidget> {
+  @override
+  Widget build(BuildContext context) {
+    const textStyle = const TextStyle(
+      fontSize: 16,
+      color: Color(0xFF212529),
+    );
+    const textFieldDecorator = const InputDecoration(
+      border: OutlineInputBorder(),
+      contentPadding: EdgeInsets.zero,
+    );
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Text(
+          'Username',
+          style: textStyle,
+        ),
+        const TextField(
+            decoration: textFieldDecorator),
+        const Text(
+          'Password',
+          style: textStyle,
+        ),
+        const TextField(decoration: textFieldDecorator),
+      ],
     );
   }
 }
