eecb523d80ce51d34560c2e7d2d557692008545c
MPREIJI <mpreiji@gmail.com>
2022-03-24 23:26:41 +0600

    24.03.1

--- lib/widgets/movie_list/movie_list_widget.dart
+++ lib/widgets/movie_list/movie_list_widget.dart
@@ -10,16 +10,68 @@ class MovieListWiget extends StatelessWidget {
       itemCount: 10,
       itemExtent: 163,
       itemBuilder: (BuildContext context, int index) {
-        return Container(
+        return Padding(
           padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
-          decoration: BoxDecoration(
-            border: Border.all(color: Colors.black.withOpacity(0.2)),
-            borderRadius: const BorderRadius.all(Radius.circular(10)),
-          ),
-          clipBehavior: Clip.hardEdge,
-          child: Row(
+          child: Stack(
             children: [
-              const Image(image: AssetImage(AppImages.film)),
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: Border.all(color: Colors.black.withOpacity(0.2)),
+                  borderRadius: const BorderRadius.all(Radius.circular(10)),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.1),
+                      blurRadius: 8,
+                      offset: const Offset(0, 2),
+                    ),
+                  ],
+                ),
+                clipBehavior: Clip.hardEdge,
+                child: Row(
+                  children: [
+                    const Image(image: AssetImage(AppImages.film)),
+                    const SizedBox(width: 15),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const SizedBox(height: 20),
+                          const Text(
+                            'Острые козырьки',
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                          const SizedBox(height: 5),
+                          const Text(
+                            'April 7, 2021',
+                            style: TextStyle(color: Colors.grey),
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                          const SizedBox(height: 20),
+                          const Text(
+                            'Британский сериал о криминальном мире Бирмингема 20-х годов прошлого века, в котором многолюдная семья Шелби стала одной из самых жестоких и влиятельных гангстерских банд послевоенного времени. Фирменным знаком группировки, промышлявшей грабежами и азартными играми, стали зашитые в козырьки лезвия.',
+                            maxLines: 2,
+                            overflow: TextOverflow.ellipsis,
+                          )
+                        ],
+                      ),
+                    ),
+                    const SizedBox(width: 15),
+                  ],
+                ),
+              ),
+              Material(
+                color: Colors.transparent,
+                child: InkWell(
+                  borderRadius: BorderRadius.circular(10),
+                  onTap: () {
+                    print('das');
+                  },
+                ),
+              ),
             ],
           ),
         );
