485d4836087ac42521a62d79cb0beac3e19a66b9
MPREIJI <mpreiji@gmail.com>
2022-04-11 13:53:55 +0600

    11.04

--- lib/widgets/main_screen/main_screen_main_info_widget.dart
+++ lib/widgets/main_screen/main_screen_main_info_widget.dart
@@ -1,18 +1,58 @@
 import 'package:flutter/material.dart';
 import 'package:themoviedb/resources/resources.dart';
+import 'package:themoviedb/widgets/elements/radial_percent_widget.dart';
 
 class MainScreenMainInfoWidget extends StatelessWidget {
   const MainScreenMainInfoWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: [
-      const _TopPosterWidget(),
-      const Padding(
-        padding: EdgeInsets.all(20.0),
-        child: _MovieNameWidget(),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const _TopPosterWidget(),
+        const Padding(
+          padding: EdgeInsets.all(20.0),
+          child: _MovieNameWidget(),
+        ),
+        const _ScoreWidget(),
+        const _SummeryWidget(),
+        Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: _OverViewWidget(),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: _DescriptionWidget(),
+        ),
+        const SizedBox(
+          height: 30,
+        ),
+        const _PeopleWidgets(),
+      ],
+    );
+  }
+
+  Text _DescriptionWidget() {
+    return const Text(
+      'Стивен Грант, кроткий сотрудник магазина подарков страдает от провалов в памяти и его посещают воспоминания о другой жизни. Обнаружив, что у него диссоциативное расстройство личности, Стивен узнает, что делит тело с наёмником Марком Спектром. Получив силу от бога Луны Хонсу, они должны разобраться в своих сложных отношениях и разгадать тайну могущественных богов Египта.',
+      style: TextStyle(
+        color: Colors.white,
+        fontSize: 16,
+        fontWeight: FontWeight.w600,
       ),
-    ]);
+    );
+  }
+
+  Text _OverViewWidget() {
+    return const Text(
+      'Overview',
+      style: TextStyle(
+        color: Colors.white,
+        fontSize: 16,
+        fontWeight: FontWeight.w600,
+      ),
+    );
   }
 }
 
@@ -40,22 +80,176 @@ class _MovieNameWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return RichText(
       maxLines: 3,
+      textAlign: TextAlign.center,
       text: const TextSpan(children: [
         TextSpan(
           text: 'Острые козырьки',
           style: TextStyle(
-            fontSize: 21,
+            color: Colors.white,
+            fontSize: 17,
             fontWeight: FontWeight.w600,
           ),
         ),
         TextSpan(
           text: ' (2021)',
           style: TextStyle(
-            fontSize: 17,
+            color: Colors.white,
+            fontSize: 16,
             fontWeight: FontWeight.w400,
           ),
         ),
       ]),
+    );
+  }
+}
+
+class _ScoreWidget extends StatelessWidget {
+  const _ScoreWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        TextButton(
+            onPressed: () {},
+            child: Row(
+              children: [
+                RadialPercentWidget(
+                  child: const Text('72'),
+                  percent: 72,
+                  fillColor: fillColor,
+                  lineColor: lineColor,
+                  freeColor: freeColor,
+                  lineWidth: lineWidth,
+                ),
+                const Text('User score'),
+              ],
+            )),
+        Container(width: 1, height: 15, color: Colors.grey),
+        TextButton(
+            onPressed: () {},
+            child: Row(
+              children: [
+                const Icon(Icons.play_arrow),
+                const Text('Play trailer'),
+              ],
+            )),
+      ],
+    );
+  }
+}
+
+class _SummeryWidget extends StatelessWidget {
+  const _SummeryWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const ColoredBox(
+      color: Color.fromRGBO(22, 21, 25, 1.0),
+      child: Padding(
+        padding: EdgeInsets.symmetric(vertical: 10, horizontal: 70),
+        child: Text(
+            'TV-14 Боевик и Приключения, НФ и Фэнтези, детектив, драма 47m',
+            maxLines: 3,
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 16,
+              fontWeight: FontWeight.w400,
+            )),
+      ),
+    );
+  }
+}
+
+class _PeopleWidgets extends StatelessWidget {
+  const _PeopleWidgets({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    const nameStyle = const TextStyle(
+      color: Colors.white,
+      fontSize: 16,
+      fontWeight: FontWeight.w400,
+    );
+
+    const jobTitleStyle = const TextStyle(
+      color: Colors.white,
+      fontSize: 16,
+      fontWeight: FontWeight.w400,
+    );
+
+    return Column(
+      children: [
+        Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  'Stefano Sollima',
+                  style: nameStyle,
+                ),
+                const Text(
+                  'Director',
+                  style: jobTitleStyle,
+                ),
+              ],
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  'Stefano Sollima',
+                  style: nameStyle,
+                ),
+                const Text(
+                  'Director',
+                  style: jobTitleStyle,
+                ),
+              ],
+            ),
+          ],
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  'Stefano Sollima',
+                  style: nameStyle,
+                ),
+                const Text(
+                  'Director',
+                  style: jobTitleStyle,
+                ),
+              ],
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  'Stefano Sollima',
+                  style: nameStyle,
+                ),
+                const Text(
+                  'Director',
+                  style: jobTitleStyle,
+                ),
+              ],
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
