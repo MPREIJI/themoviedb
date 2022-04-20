50969d4c01b8772db97433443524566536327ade
MPREIJI <mpreiji@gmail.com>
2022-04-20 22:14:34 +0600

    20.04.1

--- lib/resources/app_images.dart
+++ lib/resources/app_images.dart
@@ -3,6 +3,7 @@ part of 'resources.dart';
 class AppImages {
   AppImages._();
 
+  static const String actor = 'images/actor.jpg';
   static const String film = 'images/film.jpg';
   static const String topHeader = 'images/topHeader.jpg';
   static const String topHeaderSubImage = 'images/topHeaderSubImage.jpg';
--- lib/widgets/movie_details/movie_details_main_screen_cast_widget.dart
+++ lib/widgets/movie_details/movie_details_main_screen_cast_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:themoviedb/resources/resources.dart';
 
 class MovieDetailsMainScreenCastWidget extends StatelessWidget {
   const MovieDetailsMainScreenCastWidget({Key? key}) : super(key: key);
@@ -10,11 +11,46 @@ class MovieDetailsMainScreenCastWidget extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          const Text('Series Cast'),
-          ListView(),
-          TextButton(
-            onPressed: () {},
-            child: const Text('Full Cast & Crew'),
+          const Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Text(
+              'Series Cast',
+              style: TextStyle(
+                fontSize: 17,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 200,
+            child: Scrollbar(
+              child: ListView.builder(
+                itemCount: 20,
+                itemExtent: 120,
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (BuildContext context, int index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      children: [
+                        const Image(
+                          image: AssetImage(
+                            AppImages.actor,
+                          ),
+                        )
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(3.0),
+            child: TextButton(
+              onPressed: () {},
+              child: const Text('Full Cast & Crew'),
+            ),
           ),
         ],
       ),
